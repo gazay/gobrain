3882d20188bcc9d7a71db329833b7ecabd106091
brainopia <ravwar@gmail.com>
2008-10-18 05:01:47 +0400

    Add rooms controller

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,15 +1,13 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
-
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '15ff5515c95a72595dbd0cac6eb3189e'
+  helper :all 
+  protect_from_forgery
   
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  def help
+    Helper.instance
+  end
+
+  class Helper
+    include Singleton
+    include ActionView::Helpers::JavaScriptHelper
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def javascript_include_token
+    "<script type='text/javascript'> window.token = '#{form_authenticity_token}' </script>" if protect_against_forgery?
+  end
 end
