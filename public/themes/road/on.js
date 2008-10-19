9fd4c42dd8ade8260f447621ab99fa426268c7a0
Andrey A.I. Sitnik <andrey@sitnik.ru>
2008-10-20 01:51:49 +0400

    Rooster on road can sing

--- public/themes/road/on.js
+++ public/themes/road/on.js
@@ -1,6 +1,21 @@
+rooster = function() {
+    Preved.play('/themes/road/rooster.mp3')
+}
+
 $(document).ready(function() {
     $('body').css('background', '#7ca2ab url(/themes/road/background.jpg) no-repeat 0 100%')
     Style.add('full body', function() {
         $('body').css('height', $(document).height())
     })
+    
+    $('<div />').appendTo('body').attr('id', 'rooster')
+        .css('position', 'absolute')
+        .css('bottom', 25).css('left', 0)
+        .css('width', 140).css('height', 115)
+        .css('cursor', 'pointer')
+        .click(function() {
+            Preved.broadcast('rooster')
+        })
+      
+    Preved.server.add('rooster', rooster)
 })
--- public/themes/sky/off.js
+++ public/themes/sky/off.js
@@ -2,4 +2,6 @@ $(document).ready(function() {
     $('body').css('background', 'none')
     $('#rocket-trace').remove()
     Style.remove('rocket')
+    Preved.server.remove('start')
+    delete Rocket
 })
