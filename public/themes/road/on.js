438344b67a68e408a445b08b079bf4faefbdba01
Andrey A.I. Sitnik <andrey@sitnik.ru>
2008-10-20 01:34:08 +0400

    JS bugfixes 2

--- public/themes/road/on.js
+++ public/themes/road/on.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('body').css('background', '#7ca2ab url(/themes/road/background.jpg) no-repeat 0 100%')
     Style.add('full body', function() {
-        $('body').css('height', $(window).height())
+        $('body').css('height', $(document).height())
     })
 })
